7ff2d7d045bf2027056ee76c22ada3764c2ec6bd
mohanadkandil <mohanadmkandil@gmail.com>
2022-09-09 14:32:58 +0200

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+import { close, logo, menu } from "../assets"
+
+import { navLinks } from "../constants"
+
+const Navbar = () => {
+  return (
+    <nav className="w-full flex py-6 items-center justify-center navbar"></nav>
+  )
+}
+
+export default Navbar
---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar"
+import Billing from "./Billing"
+import Business from "./Business"
+import Button from "./Button"
+import CardDeal from "./CardDeal"
+import Clients from "./Clients"
+import CTA from "./CTA"
+import Feedback from "./Feedback"
+import Footer from "./Footer"
+import GetStarted from "./GetStarted"
+import Hero from "./Hero"
+import Stats from "./Stats"
+import Testimonials from "./Testimonials"
+
+export {
+  Navbar,
+  Billing,
+  Business,
+  Button,
+  CardDeal,
+  Clients,
+  CTA,
+  Feedback,
+  Footer,
+  GetStarted,
+  Hero,
+  Stats,
+  Testimonials,
+}
